1bfcd7e0cd2654917ccac8943ac0875f0899aab7
Santosh Reddy Nancharla <santosh.cool.reddy@gmail.com>
2018-08-21 12:24:57 -0400

    dynamic node and relation creator

--- NodeGenerator.R
+++ NodeGenerator.R
@@ -1,10 +1,15 @@
 library(shiny)
 library(RNeo4j)
-options(shiny.maxRequestSize=30*1024^2)
-graph = startGraph("http://localhost:7474/db/data/", username="neo4j", password="nsr")
+library(data.table)
+library(rjson)
+
+config <- fromJSON(file="config.json")
+options(shiny.maxRequestSize=(config$maxRequestSize*1024^2))
+graph = startGraph(config$graphUrl, username=config$username, password=config$password)
+
 
 # Define UI for data upload app ----
-ui <- fluidPage(
+createNodeUI <- fluidPage(
   
   # App title ----
   titlePanel("Uploading Files"),
@@ -15,39 +20,16 @@ ui <- fluidPage(
     # Sidebar panel for inputs ----
     sidebarPanel(
       
+      selectInput("entityLabel","Entity Type", config$labels),
+      
       # Input: Select a file ----
-      fileInput("file1", "Choose CSV File",
-                multiple = FALSE,
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
-                           ".csv")),
+      fileInput("file1", "Choose CSV File", multiple = FALSE, accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
       
       # Horizontal line ----
       tags$hr(),
       
-      # Input: Checkbox if file has header ----
-      checkboxInput("header", "Header", TRUE),
-      
-      # Input: Select separator ----
-      radioButtons("sep", "Separator",
-                   choices = c(Comma = ",",
-                               Semicolon = ";",
-                               Tab = "\t"),
-                   selected = "\t"),
-      
-      # Input: Select quotes ----
-      radioButtons("quote", "Quote",
-                   choices = c(None = "",
-                               "Double Quote" = '"',
-                               "Single Quote" = "'"),
-                   selected = '"'),
-      
-      # Horizontal line ----
-      tags$hr(),
-      
-      uiOutput("properties"),
-      uiOutput("nameFieldProperty"),
-      uiOutput("constraint"),
+      uiOutput("requiredFields"),
+
       uiOutput("uploadBtn")
       
     ),
@@ -62,8 +44,59 @@ ui <- fluidPage(
   )
 )
 
+
+
+createRelationshipUI <- fluidPage(
+  # App title ----
+  titlePanel("Creating Relationships"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      selectInput("node1","Select Node 1", config$labels),
+      selectInput("node2","Select Node 2", config$labels),
+      
+      uiOutput("node1Fields"),
+      uiOutput("node2Fields"),
+      
+      textInput("relationshipName", "Enter relationship name"),
+      
+      uiOutput("createRelationElement")
+      
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Data file ----
+      #uiOutput("relationshipUI")
+      
+    )
+  )
+  
+)
+
+
+
 # Define server logic to read selected file ----
 server <- function(input, output, session) {
+  
+  
+  output$node1Fields <- renderUI({
+    selectInput("node1Fields","Select a property from node 1", config[[input$node1]])
+  })
+  
+  output$node2Fields <- renderUI({
+    selectInput("node2Fields","Select a property from node 2", config[[input$node2]])
+  })
+  
+  output$createRelationElement <- renderUI({
+    actionButton("createRelationBtn", "Generate Relationships", class = "btn-primary")
+  })
   
   output$contents <- renderTable({
     
@@ -77,92 +110,78 @@ server <- function(input, output, session) {
     # having a comma separator causes `read.csv` to error
     tryCatch(
       {
-        df <- read.csv(input$file1$datapath,
-                       header = input$header,
-                       sep = input$sep,
-                       quote = input$quote)
+        df <- fread(input$file1$datapath)
       },
       error = function(e) {
-        # return a safeError if a parsing error occurs
-        
         stop(safeError(e))
       }
     )
-    output$properties <- renderUI({
+
+    output$requiredFields <- renderUI({
       if (is.null(df)) return(NULL)
-      items=names(df)
-      names(items)=items
-      selectInput("properties","Select properties",items,multiple=TRUE)
-    })
-    
-    output$nameFieldProperty <- renderUI({
-      if (is.null(df)) return(NULL)
-      items=names(df)
-      names(items)=items
-      selectInput("nameFieldProperty","Select name of the node from properties",items)
-    })
-    
-    output$constraint <- renderUI({
-      if (is.null(df)) return(NULL)
-      items=names(df)
-      names(items)=items
-      selectInput("constraint","Select constraint which acts as unique key from properties",items)
-    })
-    
-    #show upload button only when properties are selected
-    observe({
-      x <- input$properties
-      
-      output$uploadBtn <- renderUI({
-        if (is.null(x) || length(x)==0) {
-          x <- character(0)
-          return(NULL)
-        } else {
-          actionButton("upload", "Upload", class = "btn-primary") 
-        }
+      lapply(1:length(config[[input$entityLabel]]), function(i) {
+        selectInput(config[[input$entityLabel]][i], paste0('Select ', config[[input$entityLabel]][i]), names(df))
       })
     })
     
-    
-    
+    output$uploadBtn <- renderUI({
+      if (is.null(df)) return(NULL)
+        actionButton("upload", "Upload", class = "btn-primary")
+    })
+
     observeEvent(input$upload, {
-      x <- input$properties
       progress <- Progress$new(session, min=1, max=nrow(df))
       on.exit(progress$close())
       
-      progress$set(message = 'Uploading OMIM data to Neo4j',
-                   detail = 'This may take a while...')
-      
-      nameField = input$nameFieldProperty
-      if(is.null(nameField)){
-        stop(safeError("Please select a name field"))
-      }
-      constraint = input$constraint
-      df <- df[, x]
+      progress$set(message = paste("Uploading ",input$entityLabel," data to Neo4j"), detail = 'This may take a while...')
+
       for (i in 1:nrow(df)) {
         propertiesList <- list()
-        for (j in names(df)) {
-          if(j==nameField){
-            propertiesList[["name"]] <- df[[j]][i]
-          }else if(j==constraint){
-            propertiesList[["id"]] <- df[[j]][i]
-          }else{
-            propertiesList[[j]] <- df[[j]][i]
-          }
+        for (field in config[[input$entityLabel]]) {
+          propertiesList[[field]] <- df[[input[[field]]]][i]
         }
-        #createNode(graph, "OMIM", name=df$Symbol[i], id=df$MimNumber[i], prefix=df$Prefix[i])
-        createNode(graph, "OMIM", propertiesList)
+        createNode(graph, input$entityLabel, propertiesList)
         progress$set(value = i)
-        #Sys.sleep(0.5)
-      }
-      if(!is.null(constraint)){
-        addConstraint(graph, "OMIM", constraint) 
       }
     })
+    
     return(head(df))
   })
   
+  observeEvent(input$createRelationBtn, {
+    label1 <- input$node1
+    label2 <- input$node2
+    field1 <- input$node1Fields
+    field2 <- input$node2Fields
+    query <- paste0("MATCH (l1:",label1,") RETURN l1.",field1)
+    label1Ids <- cypherToList(graph, query)
+    count <- 0
+    for(i in label1Ids){
+      query <- paste0("MATCH (l1:",label1,") WHERE l1.",field1," = ",i," RETURN l1")
+      leftNodes <- cypherToList(graph, query)
+      query <- paste0("MATCH (l2:",label2,") WHERE l2.",field2," = ",i," RETURN l2")
+      rightNodes <- cypherToList(graph, query)
+      if(length(rightNodes)==0){
+        next
+      }
+      for (leftNode in leftNodes) {
+        for (rightNode in rightNodes) {
+          createRel(leftNode$l1, input$relationshipName, rightNode$l2)
+        }
+      }
+      count <- count+1
+      if(count==3){
+        break
+      }
+    }
+  })
+  
 }
+
+ui <- fluidPage(tabsetPanel(
+  tabPanel("Create Nodes", createNodeUI),
+  tabPanel("Create Relationships", createRelationshipUI),
+  tabPanel("Query", "contents")))
 
 # Create Shiny app ----
 shinyApp(ui, server)
