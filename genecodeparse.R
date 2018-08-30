971e1ecc4efd89793e587b969a474eb321a23b25
Tenkale <Akshay.Tenkale@cchmc.org>
2018-08-30 11:13:17 -0400

    Genecode with all parser

--- genecodeparse.R
+++ genecodeparse.R
@@ -1,10 +1,16 @@
 require(rtracklayer)
-gtf <- readGFF("C:/Users/tenr2y/Desktop/Genecode/gencode.v28lift37.annotation.gtf", version=2L)
-s1 <- sapply(strsplit(unlist(gtf["gene_id"]), split='.', fixed=TRUE), function(x) (x[1]))
-gtf["gene_id"] <- s1
-write.csv(gtfs, file = "C:/Users/tenr2y/Desktop/Genecode/genecodexon.csv",row.names=FALSE)
 require(data.table)
-gtf <- as.data.table(gtf)
-gtfs <- gtf[type == 'exon']
 
-gtfs
+gtf <- readGFF("C:/Users/tenr2y/Desktop/Genecode/gencode.v28lift37.annotation.gtf", version=2L)
+
+format_gene <- sapply(strsplit(unlist(gtf["gene_id"]), split='.', fixed=TRUE), function(x) (x[1]))
+gtf["gene_id"] <- format_gene
+
+gtf <- as.data.table(gtf)
+gtfgene <- gtf[type == 'gene']
+gtftrans <- gtf[type == 'transcript']
+gtfexon <- gtf[type == 'exon']
+write.csv(gtfgene, file = "C:/Users/tenr2y/Desktop/Genecode/genecode_gene1.csv",row.names=FALSE)
+write.csv(gtftrans, file = "C:/Users/tenr2y/Desktop/Genecode/genecode_trans1.csv",row.names=FALSE)
+write.csv(gtfexon, file = "C:/Users/tenr2y/Desktop/Genecode/genecode_exon1.csv",row.names=FALSE)
+
